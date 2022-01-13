5d72c01d5d35f1773c003d6cd8b0591efb346ba9
Paloma Lins <palomalimadl@gmail.com>
2022-01-13 10:15:47 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Paloma"
+let numero = "15"
+let ano = 2022 - (numero)
+console.log(ano)
+let anoSomado = 2022 + 15
+console.log (anoSomado)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true 
+console.log(typeof quartaFeira) 
